f0c96ca480eb1a59e685d6c2c15b8a5ccc7140e5
D4ng3r25 <silverasebastian82@gmail.com>
2024-04-30 18:11:16 +0200

    rankin,registro,combate completos

---
+++ Test/PersonajeUserTest.java
@@ -0,0 +1,116 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonajeUserTest {
+
+    @Test
+    void sumarPotencialAtaque() {
+
+
+    }
+
+    @Test
+    void sumarPotencialDefensa() {
+    }
+
+    @Test
+    void estaVivo() {
+    }
+
+    @Test
+    void oroSufiencte() {
+    }
+
+    @Test
+    void setPersonaje() {
+    }
+
+    @Test
+    void getPersonaje() {
+    }
+
+    @Test
+    void setArmaIzq() {
+    }
+
+    @Test
+    void setArmaDer() {
+    }
+
+    @Test
+    void getManosLibres() {
+    }
+
+    @Test
+    void mostrarArmas() {
+    }
+
+    @Test
+    void mostrarArmadura() {
+    }
+
+    @Test
+    void notieneArmadura() {
+    }
+
+    @Test
+    void setArmadura() {
+    }
+
+    @Test
+    void mostrarTodasArmas() {
+    }
+
+    @Test
+    void getArma() {
+    }
+
+    @Test
+    void existeEquipo() {
+    }
+
+    @Test
+    void manosSuficientes() {
+    }
+
+    @Test
+    void mostrarTodasArmaduras() {
+    }
+
+    @Test
+    void existeEquipoArmadura() {
+    }
+
+    @Test
+    void getArmadura() {
+    }
+
+    @Test
+    void restarVida() {
+    }
+
+    @Test
+    void getArmaduraActiva() {
+    }
+
+    @Test
+    void getArmaActiva() {
+    }
+
+    @Test
+    void setVida() {
+    }
+
+    @Test
+    void getVida() {
+    }
+
+    @Test
+    void sumarOro() {
+    }
+
+    @Test
+    void getOro() {
+    }
+}
